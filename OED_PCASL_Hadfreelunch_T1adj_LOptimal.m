735f851d345833d4b43cb9cd7e63bff9fe8b80d6
Joseph G. Woods <jgmbwoods@gmail.com>
2020-07-01 14:17:07 +0100

    file name change

--- OED_PCASL_Hadfreelunch_T1adj_LOptimal.m
+++ OED_PCASL_Hadfreelunch_T1adj_LOptimal.m
@@ -1,4 +1,4 @@
-function [bestPLD,bestTau,bestminVariance] = OED_PCASL_HadfreeLlnch_T1adj_LOptimal(varargin)
+function [bestPLD,bestTau,bestminVariance] = OED_PCASL_HadfreeLunch_T1adj_LOptimal(varargin)
 
 time = tic;
 
